28bd67707b204bb777ef7eac54a3b939cd95f110
Victor <victorms.adm@gmail.com>
2022-12-14 11:42:55 -0300

    migrations, valitators, module, controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
+import { PrismaService } from './prisma.service';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { HttpModule } from './http.module';
 
 @Module({
-  imports: [],
+  imports: [HttpModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [PrismaService],
 })
 export class AppModule {}
